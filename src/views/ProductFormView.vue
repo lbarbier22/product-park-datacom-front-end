6d58d3398038b90eaf97f0147028fb32a7f4eec8
lbarbier22 <barbier.lucas08@gmail.com>
2026-07-30 10:37:41 +0200

    fix : rename all reason into rejectionReason

--- src/views/ProductFormView.vue
+++ src/views/ProductFormView.vue
@@ -11,7 +11,7 @@
     />
 
     <template v-else>
-      <RejectionBanner :reason="rejectionReason" />
+      <RejectionBanner v-if="rejectionReason" :rejectionReason="rejectionReason" />
       <StepIndicator :current-step="currentStep" />
 
       <component
