bc79a76f83968f411b8582560eb219035011239f
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2026-07-30 10:02:42 +0200

    hf : missing not found vue.

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <h2>Page introuvable</h2>
+
+    <p>
+      La page que vous recherchez n'existe pas ou a été déplacée.
+    </p>
+
+    <RouterLink to="/" class="btn-home">
+      Retour à l'accueil
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped src="../styles/NotFound.css"></style>
